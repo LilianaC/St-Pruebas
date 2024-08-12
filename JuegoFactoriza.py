5fdeed6752ddfb43769c8f68eff3d3c028014353
LilianaC <liliana.castanon@gmail.com>
2024-08-11 21:34:52 -0600

    Update JuegoFactoriza.py

--- JuegoFactoriza.py
+++ JuegoFactoriza.py
@@ -21,3 +21,13 @@ st.sidebar.header("Ajustes")
 
 metric = st.sidebar.selectbox("Nivel:", ["Sencillo", "Fácil", "Experto"])
 metric2 = st.sidebar.checkbox("Nivel:", ["Sencillo", "Fácil", "Experto"])
+
+with st.form("my_form"):
+   st.write("Inside the form")
+   my_number = st.slider('Pick a number', 1, 10)
+   my_color = st.selectbox('Pick a color', ['red','orange','green','blue','violet'])
+   st.form_submit_button('Submit my picks')
+
+# This is outside the form
+st.write(my_number)
+st.write(my_color)
